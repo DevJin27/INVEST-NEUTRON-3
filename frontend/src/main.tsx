27e60cf8eccef1d40df73b9f43dc1aa865fb7f20
DevJin27 <devjindal2317@gmail.com>
2026-04-05 12:08:47 +0530

    msggg

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
+import './modern-app.css'
+import './modern-investment.css'
+import './modern-admin.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
